f81e28bcf905d8ae38b9319d53634b52b85f9734
Ashwini Kumar <aliaadmin@wtwngrok.onmicrosoft.com>
2020-08-06 15:09:37 +0530

    Updates

---
+++ precache-manifest.f725b01c30c98643cbadf0eeb2f8b357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0c15c79bfbcc78d608fec3bd546efd",
+    "url": "/Ashwakum/monsters-rolodexNew/index.html"
+  },
+  {
+    "revision": "43a6f0d74cc2efb58f12",
+    "url": "/Ashwakum/monsters-rolodexNew/static/css/main.57c7b8b8.chunk.css"
+  },
+  {
+    "revision": "620f7bdd283888331ecb",
+    "url": "/Ashwakum/monsters-rolodexNew/static/js/2.313ae473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ashwakum/monsters-rolodexNew/static/js/2.313ae473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a6f0d74cc2efb58f12",
+    "url": "/Ashwakum/monsters-rolodexNew/static/js/main.750c1382.chunk.js"
+  },
+  {
+    "revision": "dc9c4931de14ac6088ea",
+    "url": "/Ashwakum/monsters-rolodexNew/static/js/runtime-main.851358ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ashwakum/monsters-rolodexNew/static/media/logo.5d5d9eef.svg"
+  }
+]);
